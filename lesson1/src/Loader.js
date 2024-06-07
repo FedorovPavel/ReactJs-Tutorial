dd735c526b29f0d0552ca39e4935d2fc22493965
Pavel <IU7.2@yandex.ru>
2024-06-07 17:58:36 +0300

    A1

--- lesson1/src/Loader.js
+++ lesson1/src/Loader.js
@@ -1,3 +1,9 @@
 import React from 'react';
 
 export default () => <div style={{"display": 'flex', "justifyContent": 'center'}}className="lds-ring"><div></div><div></div><div></div><div></div></div>
+
+
+
+
+
+export function isOdd(val) {return val% 2 === 0};
