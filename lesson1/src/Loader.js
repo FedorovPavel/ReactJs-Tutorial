85880cb1efaed7af7f9fa00e07e79fef0acea311
Pavel <IU7.2@yandex.ru>
2024-06-11 15:46:44 +0300

    AAAA

--- lesson1/src/Loader.js
+++ lesson1/src/Loader.js
@@ -1,3 +1,4 @@
 import React from 'react';
 
 export default () => <div style={{"display": 'flex', "justifyContent": 'center'}}className="lds-ring"><div></div><div></div><div></div><div></div></div>
+
